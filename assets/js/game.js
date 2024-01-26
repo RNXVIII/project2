e6014e49a635c56ade646ff87c814bbec11a6cff
RNXVIII <>
2024-01-26 02:51:07 +0000

    added the jump function for game

---
+++ assets/js/game.js
@@ -0,0 +1,15 @@
+const dino = document.getElementById('dino');
+
+function jump() {
+    if (dino.classList != "jump") {
+        dino.classList.add("jump");
+
+        setTimeout(function () {
+            dino.classList.remove("jump");
+        }, 300);
+    }
+}
+
+document.addEventListener('click', function (event) {
+    jump();
+});
